a00807143f8e5fce2f192a5990ff310c039da5bb
Valant13 <valant13.zt@gmail.com>
2025-02-22 22:03:13 +0200

    Revert "Test file loading"
    This reverts commit 32d317e75ebdd82756593eab0ae2ae2d21643605.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,7 @@
 import { promises as fs } from 'fs';
 
-export default async function Page(props: {
-  searchParams?: Promise<{
-    mode?: string;
-  }>;
-}) {
-  let file = '';
-  const searchParams = await props.searchParams;
-  if (searchParams?.mode === '1') {
-    file = await fs.readFile(process.cwd() + '/app/rustplus.proto', 'utf8');
-  }
+export default async function Page() {
+  const file = await fs.readFile(process.cwd() + '/app/rustplus.proto', 'utf8');
 
   return (
     <div>
