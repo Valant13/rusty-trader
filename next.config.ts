6b344f50da7815411f9811c24f06d8416a2ed72c
Valant13 <valant13.zt@gmail.com>
2025-02-22 22:03:12 +0200

    Revert "Test file loading"
    This reverts commit f226d1a8e5ebb9042862020fde3582e2bad6c576.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     WS_NO_BUFFER_UTIL: 'true',
-  },
-  outputFileTracingIncludes: {
-    '/api/read-proto': ['./app/rustplus.proto']
   }
 };
 
