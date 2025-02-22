f226d1a8e5ebb9042862020fde3582e2bad6c576
Valant13 <valant13.zt@gmail.com>
2025-02-22 21:56:39 +0200

    Test file loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     WS_NO_BUFFER_UTIL: 'true',
+  },
+  outputFileTracingIncludes: {
+    '/api/read-proto': ['./app/rustplus.proto']
   }
 };
 
