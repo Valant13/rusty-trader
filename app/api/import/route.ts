ff88178e26720e0f30539b8fc84f272238c98530
Valant13 <valant13.zt@gmail.com>
2025-02-28 00:56:24 +0200

    Split item folders alphabetically to bypass the 1000 blobs listing limit

--- app/api/import/route.ts
+++ app/api/import/route.ts
@@ -1,19 +1,19 @@
-import {list, ListBlobResultBlob} from "@vercel/blob";
+import {list} from "@vercel/blob";
 import { NextResponse } from 'next/server';
 import {Item} from "@/app/lib/definitions";
 import {deleteItems, saveItems} from "@/app/lib/data";
+import path from "path";
 
 export async function GET() {
-  const { blobs } = await list({ prefix: 'items/' });
+  console.log('Fetching data...');
+  const itemsData = await fetchJsonFiles('items/data/');
+  console.log('Done!');
 
-  const itemsData = await fetchJsonFiles(blobs);
-  const imageUrls = await fetchImageUrls(blobs);
+  process.stdout.write('Fetching images...');
+  const imageUrlMap = await fetchImageUrls('items/images/');
+  console.log('Done!');
 
-  const items = itemsData.map(data => {
-    const imagePath = 'items/' + data.shortname + '.png';
-
-    return prepareItem(data, imageUrls[imagePath]);
-  });
+  const items = itemsData.map(data => prepareItem(data, imageUrlMap));
 
   await deleteItems();
   await saveItems(items);
@@ -21,28 +21,53 @@ export async function GET() {
   return NextResponse.json(itemsData.map(data => data.shortname));
 }
 
-async function fetchJsonFiles(blobs: ListBlobResultBlob[]) {
-  const jsonBlobs = blobs.filter((blob) => blob.pathname.endsWith('.json'));
+async function fetchJsonFiles(prefix: string) {
+  const blobs = await listFiles(prefix);
 
-  return Promise.all(
-    jsonBlobs.map(async (blob) => {
-      const response = await fetch(blob.url);
+  const responses = await Promise.all(
+    blobs.map(blob => fetch(blob.url).then(response => {
+      console.log(' ', blob.pathname);
 
-      return response.json();
-    })
+      return response;
+    }))
   );
+
+  return Promise.all(responses.map(response => response.json()));
 }
 
-async function fetchImageUrls(blobs: ListBlobResultBlob[]) {
-  const imageBlobs = blobs.filter((blob) => blob.pathname.endsWith('.png'));
+async function fetchImageUrls(prefix: string) {
+  const blobs = await listFiles(prefix);
 
-  return Object.fromEntries(imageBlobs.map(({ pathname, url }) => [pathname, url]));
+  return Object.fromEntries(blobs.map(
+    ({ pathname, url }) => [pathname, url]
+  ));
 }
 
-function prepareItem(data: any, imageUrl: string): Item {
+async function listFiles(prefix: string) {
+  const splitDirs = Array.from('abcdefghijklmnopqrstuvwxyz');
+  const allBlobs = [];
+
+  const results = await Promise.all(
+    splitDirs.map(
+      dir => list({ prefix: path.join(prefix, dir) + '/' })
+    )
+  );
+
+  for (const result of results) {
+    const { blobs } = result;
+    allBlobs.push(...blobs);
+  }
+
+  return allBlobs;
+}
+
+function prepareItem(data: any, imageUrlMap: {[k: string]: string}): Item {
+  const shortName = data.shortname;
+  const imagePath = `items/images/${shortName[0]}/${shortName}.png`;
+
   return {
     itemId: data.itemid,
-    imageUrl: imageUrl,
+    imageUrl: imageUrlMap[imagePath] ?? '/something.png',
     name: data.Name,
   };
 }
--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,26 +1,65 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
-import {put} from '@vercel/blob';
+import {del, list, put} from '@vercel/blob';
 
 export async function GET() {
-  const filenames = fs.readdirSync('var/items');
-  const uploadPromises = [];
+  process.stdout.write('Deleting old files...');
+  await deleteFiles('items/');
+  console.log('Done!');
 
-  for (const filename of filenames) {
-    if (filename === '.gitkeep') {
-      continue;
+  console.log('Uploading JSON files...');
+  const dataPaths = await uploadFiles('var/items', '.json', 'items/data', 100);
+  console.log('Done!');
+
+  console.log('Uploading images...');
+  const imagePaths = await uploadFiles('var/items', '.png', 'items/images', 100);
+  console.log('Done!');
+
+  return NextResponse.json({
+    'data': dataPaths,
+    'images': imagePaths,
+  });
+}
+
+async function deleteFiles(pathname: string) {
+  while (true) {
+    const { blobs } = await list({ prefix: pathname });
+
+    if (!blobs.length) {
+      break;
     }
 
-    const localPathname = path.join('var/items', filename);
-    const blobPathname = path.join('items', filename);
+    await Promise.all(blobs.map(blob =>
+      del(blob.url)
+        .then(() => console.log(' ', blob.pathname)),
+    ));
+  }
+}
 
-    const fileData = fs.readFileSync(localPathname);
+async function uploadFiles(localDir: string, extension: string, blobDir: string, batchSize: number) {
+  const filenames = fs.readdirSync(localDir).filter((f) => f.endsWith(extension));
+  const result = [];
 
-    uploadPromises.push(put(blobPathname, fileData, { access: 'public' }));
+  for (let i = 0; i < filenames.length; i += batchSize) {
+    const batch = filenames.slice(i, i + batchSize);
+
+    result.push(
+      ...await Promise.all(batch.map(filename => {
+        const localPath = path.join(localDir, filename);
+        const blobPath = path.join(blobDir, filename[0], filename);
+
+        const file = fs.readFileSync(localPath);
+
+        return put(blobPath, file, { access: 'public' })
+          .then(blob => {
+            console.log(' ', blob.pathname);
+
+            return blob.pathname;
+          });
+      }))
+    );
   }
 
-  const blobs = await Promise.all(uploadPromises);
-
-  return NextResponse.json(blobs);
+  return result;
 }
